9c38deecfb6282344744aeb157a939a4b7e2a3a9
arnavsub <arnav.sub@gmail.com>
2024-06-04 18:40:28 -0700

    fixed the deadline sort function

--- src/DeadlineSortTask.cpp
+++ src/DeadlineSortTask.cpp
@@ -1,5 +1,6 @@
 #include "../header/DeadlineSortTask.hpp"
 #include "../header/task.hpp"
+#include <algorithm>
 
 DeadlineSortTask::DeadlineSortTask() {
     //empty
@@ -11,25 +12,8 @@ DeadlineSortTask::~DeadlineSortTask() {
 
 
 void DeadlineSortTask::DeadlineSort(vector<Task>& taskList) {
-    int len = taskList.size();
-    bool swapped;
-    
-    for (int i = 0; i < len - 1; i++) {
-        swapped = false;
-        
-        for (int j = 0; j < len - 1 - i; j++) {
-            string prev = taskList.at(j).getDeadline();
-            string curr = taskList.at(j + 1).getDeadline();
-            if (prev > curr) {
-                swap(prev, curr);
-                swapped = true;
-            }
-        }
-
-        // If no two elements were swapped by inner loop, then break
-        if (!swapped) {
-            break;
-        }
-    }
+        sort(taskList.begin(), taskList.end(), [](const Task &first, const Task &second) {
+            return first.getDeadline() < second.getDeadline(); // Assumes deadlines can be compared as strings; adjust if necessary
+        });
 }
 
